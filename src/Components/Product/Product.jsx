7e1472a4fa50e0e23e050eca6026139caef0ddce
gogis <bbdostoevskij@gmail.com>
2022-09-20 01:15:29 +0300

    add stars

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,47 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.sass";
 import PlanshetImage from "../../Images/ProductImages/Planshet.png";
 import ProcImage from "../../Images/ProductImages/Proc.png";
 import ProcImage1 from "../../Images/ProductImages/Proc1.png";
+import Stars from "./Stars/Stars";
+import StarsNew from "../StarsNew/StarsNew";
 // import image1 from "../../Images/ProductImages/Proc.png";
 // import "../../Images/ProductImages/Proc1.png";
 // import "../../Images/ProductImages/Planshet.png";
 // import Stars from "./Stars/Stars";
 
-const Prod = [
-  {
-    id: 0,
-    image: ProcImage,
-    title: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
-    oldPrice: "$499.00",
-    newPrice: "$499.00",
-  },
-  {
-    id: 1,
-    image: ProcImage1,
-    title: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
-    oldPrice: "$799.00",
-    newPrice: "$799.00",
-  },
-  {
-    id: 2,
-    image: PlanshetImage,
-    title: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
-    oldPrice: "$999.00",
-    newPrice: "$999.00",
-  },
-];
-
 const Product = () => {
+  // const [selectedProductItem, setSelectedProductItem] = useState({});
+  const productArray = [
+    {
+      id: 0,
+      image: ProcImage,
+      title: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+      oldPrice: "$499.00",
+      newPrice: "$499.00",
+    },
+    {
+      id: 1,
+      image: ProcImage1,
+      title: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+      oldPrice: "$799.00",
+      newPrice: "$799.00",
+    },
+    {
+      id: 2,
+      image: PlanshetImage,
+      title: "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+      oldPrice: "$999.00",
+      newPrice: "$999.00",
+    },
+  ];
+
+  const selectProduct = (prod) => {
+    // setSelectedProductItem(prod);
+  };
   return (
     <div className="ProductContainer">
-      {Prod.map((prod) => {
+      {/* <div className="ProductTitle">
+        <h1 className="ProductTitle-Title">New Products</h1>
+        <a className="ProductTitle-SeeAll" href="google.com">
+          See All New Products
+        </a>
+      </div> */}
+      {productArray.map((prod) => {
         return (
-          <div className="Product" key={prod.id}>
+          <div
+            className={prod.active ? "Product Product-active" : "Product"}
+            key={prod.id}
+            onClick={() => selectProduct(prod)}
+          >
             <div className="ProductImageContainer">
               <img className="ProductImage" src={prod.image} alt="img"></img>
             </div>
-            <div className="ProductRating">{/* <Stars /> */}</div>
+            <div className="ProductRating">
+              <StarsNew />
+            </div>
             <div className="ProductDescription">
               <p>{prod.title}</p>
             </div>
@@ -57,3 +75,14 @@ const Product = () => {
 };
 
 export default Product;
+
+//  Крок 1.
+//    При кліці на товар має викликатись функція selectProduct.
+//  Крок 2.
+//    У цю функцію параметром має передаватися об'єкт клікнутого товару.
+//  Крок 3.
+//    Використовцючи консоль лог в цій функції превірити правильність вибору товару.
+//  Крок 4.
+//    Використовуючи setSelectedProductItem (setState) записати це значення в selectedProductItem.
+
+// Доробити зірочки.
--- src/Components/Product/Stars/Stars.jsx
+++ src/Components/Product/Stars/Stars.jsx
@@ -1,71 +1,66 @@
-// import React from "react";
-// import "./Stars.sass";
+import React from "react";
+import "./Stars.sass";
 
-// const Stars = () => {
-//   // const ratings = document.querySelectorAll(".rating");
-//   // if (ratings.lenght > 0) {
-//   //   initRatings();
-//   // }
+const Stars = () => {
+  const StarsArray = [
+    {
+      id: 0,
+      type: "radio",
+      className: "RatingItem",
+      name: "rating",
+      value: "1",
+    },
+    {
+      id: 1,
+      type: "radio",
+      className: "RatingItem",
+      name: "rating",
+      value: "2",
+    },
+    {
+      id: 2,
+      type: "radio",
+      className: "RatingItem",
+      name: "rating",
+      value: "3",
+    },
+    {
+      id: 3,
+      type: "radio",
+      className: "RatingItem",
+      name: "rating",
+      value: "4",
+    },
+    {
+      id: 4,
+      type: "radio",
+      className: "RatingItem",
+      name: "rating",
+      value: "5",
+    },
+  ];
 
-//   // function initRatings() {
-//   //   let ratingActive, ratingValue;
-//   //   for (let index = 0; index < ratings.length; index++) {
-//   //     // const rating = ratings[index];
-//   //     // initRating(rating);
+  return (
+    <div className="Rating">
+      <div className="RatingBody">
+        <div className="RatingActive"></div>
+        {StarsArray.map((star) => {
+          return (
+            <div className="RatingItems" key={star.id}>
+              <input
+                key={star.id}
+                type={star.type}
+                className={star.class}
+                value={star.value}
+                name={star.name}
+              ></input>
+            </div>
+          );
+        })}
+      </div>
+      <div className="RatingValue">3</div>
+    </div>
+  );
+};
 
-//   //     setRatingActiveWidth();
-//   //   }
-
-//   // function initRatingsVars(rating) {
-//   //     ratingActive = rating.querySelector(".rating__active");
-//   //     ratingValue = rating.querySelector(".rating__value");
-//   //   }
-
-//   //   function setRatingActiveWidth(index = ratingValue.innerHTML) {
-//   //     const ratingActiveWidth = index / 0.05;
-//   //     ratingActive.style.width = `${ratingActiveWidth}%`;
-//   //   }
-//   // }
-//   return (
-//     <div className="Rating">
-//       <div className="RatingBody">
-//         <div className="RatingActive"></div>
-//         <div className="RatingItems">
-//           <input
-//             type="radio"
-//             class="RatingItem"
-//             value="1"
-//             name="rating"
-//           ></input>
-//           <input
-//             type="radio"
-//             class="RatingItem"
-//             value="2"
-//             name="rating"
-//           ></input>
-//           <input
-//             type="radio"
-//             class="RatingItem"
-//             value="3"
-//             name="rating"
-//           ></input>
-//           <input
-//             type="radio"
-//             class="RatingItem"
-//             value="4"
-//             name="rating"
-//           ></input>
-//           <input
-//             type="radio"
-//             class="RatingItem"
-//             value="5"
-//             name="rating"
-//           ></input>
-//         </div>
-//       </div>
-//       <div className="RatingValue">3</div>
-//     </div>
-//   );
-// };
-
-// export default Stars;
+export default Stars;
